7ca04849d24ed374c2be23492f02d2daaa319c75
Pourya-swe <pourya.swe@gmail.com>
2025-04-27 17:35:40 +0330

    Fix issue with type casting for production

--- src/Utils/ImagePath/getImagePath.ts
+++ src/Utils/ImagePath/getImagePath.ts
@@ -1,10 +1,10 @@
 const images = import.meta.glob("/src/assets/images/*.png", { eager: true });
 
 function getImagePath(fileName: string): string {
-  const match = Object.entries(images).find(([path, mod]) =>
+  const match = Object.entries(images).find(([path]) =>
     path.includes(fileName)
   );
-  return match ? match[1].default : "";
+  return match ? (match[1] as { default: string }).default : "";
 }
 
 export default getImagePath;
