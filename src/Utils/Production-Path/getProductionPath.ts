e962d34a46d64c7e7c8860f462d19960f22122c6
Pourya-swe <pourya.swe@gmail.com>
2025-04-28 18:18:10 +0330

    Add description to it

--- src/Utils/Production-Path/getProductionPath.ts
+++ src/Utils/Production-Path/getProductionPath.ts
@@ -1,3 +1,5 @@
+// This function will grab and import the assets properly so in production, vite includes medias (picture, videos, and etc.) correctly in the project production phase.
+
 const assets = import.meta.glob(
   "/src/assets/**/*.{png,jpg,jpeg,svg,webm,mp4,webp}",
   { eager: true }
